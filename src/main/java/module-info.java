9e2a470ce46c9b7bd7da45acd7f2b1e643d293e6
Wing Yan Yeung <wingyan.yeung@mygeorgian.ca>
2023-06-25 20:14:47 -0400

    initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.assignment1comp1011wingyanyeung1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+
+    opens com.example.assignment1comp1011wingyanyeung1 to javafx.fxml;
+    exports com.example.assignment1comp1011wingyanyeung1;
+}
